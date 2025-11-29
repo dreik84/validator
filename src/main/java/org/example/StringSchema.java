371e1086d86f4d23adf14c173163f05aa727876c
dreik84 <dreik84@mail.ru>
2025-11-29 16:54:26 +0300

    Update StringSchema.java

--- src/main/java/org/example/StringSchema.java
+++ src/main/java/org/example/StringSchema.java
@@ -1,4 +1,27 @@
 package org.example;
 
 public class StringSchema {
+
+    private boolean required;
+    private int minLength;
+    private String contains;
+
+    public StringSchema required() {
+        required = true;
+        return this;
+    }
+
+    public StringSchema minLength(int len) {
+        minLength = len;
+        return this;
+    }
+
+    public StringSchema contains(String sub) {
+        contains = sub;
+        return this;
+    }
+
+    public boolean isValid(String value) {
+        return true;
+    }
 }
