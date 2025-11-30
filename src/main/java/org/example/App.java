9a0fd0881b2be9770d0dfdb8cc2d74f6fa01677b
dreik84 <dreik84@mail.ru>
2025-11-30 16:47:50 +0300

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,23 @@ public class App {
         Validator validator = new Validator();
         StringSchema schema = validator.string();
 
-        schema.isValid("");
-        schema.isValid(null);
+        System.out.println(schema.isValid(""));
+        System.out.println(schema.isValid(null));
+
+        schema.required();
+
+        System.out.println(schema.isValid(null));
+        System.out.println(schema.isValid(""));
+        System.out.println(schema.isValid("what does the fox say"));
+        System.out.println(schema.isValid("test"));
+
+        System.out.println(schema.contains("wh").isValid("what does the fox say"));
+        System.out.println(schema.contains("what").isValid("what does the fox say"));
+        System.out.println(schema.contains("whatthe").isValid("what does the fox say"));
+
+        System.out.println(schema.isValid("what does the fox say"));
+
+        StringSchema schema1 = validator.string();
+        System.out.println(schema1.minLength(10).minLength(4).isValid("Hexlet"));
     }
 }
