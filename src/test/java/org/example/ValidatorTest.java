464ef7e9921b16330b58f22a793972628c81a9d3
dreik84 <dreik84@mail.ru>
2025-12-05 16:30:55 +0300

    Update tests

--- src/test/java/org/example/ValidatorTest.java
+++ src/test/java/org/example/ValidatorTest.java
@@ -1,12 +1,31 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 class ValidatorTest {
 
+    Validator validator;
+
+    @BeforeEach
+    void initValidator() {
+        validator = new Validator();
+    }
+
     @Test
     void string() {
+        assertInstanceOf(StringSchema.class, validator.string());
+    }
+
+    @Test
+    void number() {
+        assertInstanceOf(NumberSchema.class, validator.number());
+    }
+
+    @Test
+    void map() {
+        assertInstanceOf(MapSchema.class, validator.map());
     }
 }
